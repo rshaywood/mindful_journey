ed210cbcc086e556154c604e040dc7a1df27a4b9
Rafal Jawad <rafal.qasim@gmail.com>
2022-07-22 04:12:37 -0400

    setting home.html,login.html,signup.htm and first styling

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -4,3 +4,10 @@ from flask import render_template,redirect,request,session, flash
 from flask_app.models import user,goal,activity
 
 
+# home page route
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+
