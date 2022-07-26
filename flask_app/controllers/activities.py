305dca55c0ff927db61ad5a3b7cf1d7505d569da
Trevor Ryan <08t.ryan@gmail.com>
2022-07-25 20:37:37 -0700

    Edit and add changes

--- flask_app/controllers/activities.py
+++ flask_app/controllers/activities.py
@@ -7,6 +7,7 @@ from flask_app.models.user import User
 
 # CREATE - ROUTES
 
+
 @app.route('/new/activity')
 def new_activity():
     if 'user_id' not in session:
@@ -16,6 +17,7 @@ def new_activity():
     # }
     return render_template('add_activity.html')
 
+
 @app.route('/create/activity', methods=['POST'])
 def create_activity():
     if 'user_id' not in session:
@@ -24,9 +26,11 @@ def create_activity():
         return redirect('/new/activity')
     else:
         activity.Activity.save(request.form)
+
         return redirect('/users/dashboard')
 
 # READ - ROUTES
+
 
 @app.route('/activity/<int:id>')
 def show_activity(id):
@@ -40,16 +44,18 @@ def show_activity(id):
     }
     return render_template("show_activity.html", workout=activity.Activity.get_one(data), user=User.get_by_id(user_data))
 
+
 @app.route('/journal/<int:id>')
 def show_journal(id):
     if 'user_id' not in session:
         return redirect('/')
-    the_journal= activity.Activity.get_all({"user_id":session['user_id']})
+    the_journal = activity.Activity.get_all({"user_id": session['user_id']})
     this_user = user.User.get_user_by_id(session['user_id'])
     this_activity = activity.Activity.get_one_activity(id)
     return render_template("user_activities.html", the_journal=the_journal, this_user=this_user, this_activity=this_activity)
 
 # UPDATE - ROUTES
+
 
 @app.route('/edit/activity/<int:id>')
 def show_edit_activity_form(id):
@@ -61,7 +67,7 @@ def show_edit_activity_form(id):
     user_data = {
         "id": session['user_id']
     }
-    return render_template("edit_activity.html", edit=activity.Activity.get_one(data), user=User.get_by_id(user_data))
+    return render_template("edit_activity.html", edit=activity.Activity.get_one_activity(data), user=User.get_user_by_id(user_data))
 
 
 @app.route('/update/activity', methods=['POST'])
@@ -71,10 +77,14 @@ def update_activity():
     if not activity.Activity.validate_activity(request.form):
         return redirect('/new/activity')
     data = {
+        "goal_name": request.form["goal_name"],
         "activity_name": request.form["activity_name"],
-        "id": request.form["id"]
+        "comment": request.form["comment"],
+        "feeling_before": request.form["feeling_before"],
+        "feeling_after": request.form["feeling_after"],
     }
     activity.Activity.update_activity(data)
+
     return redirect('/users/dashboard')
 
 
