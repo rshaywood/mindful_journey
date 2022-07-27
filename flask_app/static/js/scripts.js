4efe1a085b93214c9f0cbaea00bf13e84c4ca92d
Rafal Jawad <rafal.qasim@gmail.com>
2022-07-27 04:20:24 -0400

    fixed Trever's code

--- flask_app/static/js/scripts.js
+++ flask_app/static/js/scripts.js
@@ -11,11 +11,10 @@ getQuote()
 
 window.onload = choosePic;
 
-var myPix = new Array("static/images/harmony.png", "static/images/meditation.png", "static/images/MJDesign.png");
+var myPix = new Array("/static/images/ali.png", "/static/images/sailor.webp", "/static/images/bigjourney.jpg", "/static/images/inspiration.jpg", "/static/images/proud.png");
 
 function choosePic() {
     var randomNum = Math.floor(Math.random() * myPix.length);
     document.getElementById("myPicture").src = myPix[randomNum];
 }
 
-choosePic()
